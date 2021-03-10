ff418300083475d33059576e9ff1a91ef5ac2d16
levanzakharov <levanzakharov@gmail.com>
2021-03-10 19:53:58 +0400

    UI menu V3

---
+++ Arena Game/Assets/Scripts/HelpMethods/HelpMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HelpMethods
+{
+    public static string RandomString(int length)
+    {
+        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        var stringChars = new char[length];
+        var random = new System.Random();
+
+        for (int i = 0; i < stringChars.Length; i++)
+        {
+            stringChars[i] = chars[random.Next(chars.Length)];
+        }
+
+        var finalString = new String(stringChars);
+
+
+        return finalString;
+    }
+}
--- Arena Game/Assets/Scripts/Server/Launcher.cs
+++ Arena Game/Assets/Scripts/Server/Launcher.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.UI;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
@@ -11,7 +12,12 @@ public class Launcher : MonoBehaviourPunCallbacks
     [SerializeField]
     private byte maxPlayersPerRoom = 4;
 
+    [Tooltip("The Ui InputField to let the user see room name")]
+    [SerializeField]
+    private InputField RoomNameField;
+    private string RoomName;
 
+    //wasashlelia testirebis mere
     [Tooltip("The Ui Panel to let the user enter name, connect and play")]
     [SerializeField]
     private GameObject controlPanel;
@@ -24,6 +30,8 @@ public class Launcher : MonoBehaviourPunCallbacks
     [Tooltip("The UI Label to inform the user that the connection is in progress")]
     [SerializeField]
     private GameObject progressLabel;
+
+    //-----------------------------------------
 
 
     [Tooltip("The UI Label to inform the user that the connection is in progress")]
@@ -57,8 +65,10 @@ public class Launcher : MonoBehaviourPunCallbacks
         // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
         PhotonNetwork.AutomaticallySyncScene = true;
 
-        Debug.Log("hello");
         Menu.OpenMenu("Control Panel");
+
+        RoomName = HelpMethods.RandomString(4);
+        RoomNameField.text = RoomName;
         //MenuButtons(0);
     }
 
@@ -102,6 +112,7 @@ public class Launcher : MonoBehaviourPunCallbacks
         {
             // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
             PhotonNetwork.JoinRandomRoom();
+
         }
         else
         {
@@ -117,13 +128,24 @@ public class Launcher : MonoBehaviourPunCallbacks
         
     }
 
+    public void CreateRoom()
+    {
+        if (string.IsNullOrEmpty(RoomNameField.text) || string.IsNullOrEmpty(RoomName))
+        {
+            return;
+        }
+
+        PhotonNetwork.CreateRoom(RoomName, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+        Menu.OpenMenu("Connecting...");
+    }
+
 
     #endregion
 
     #region MonoBehaviourPunCallbacks Callbacks
     public override void OnConnectedToMaster()
     {
-        Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+        Debug.Log("OnConnectedToMaster()");
 
         // we don't want to do anything if we are not attempting to join a room.
         // this case where isConnecting is false is typically when you lost or quit the game, when this level is loaded, OnConnectedToMaster will be called, in that case
@@ -136,7 +158,6 @@ public class Launcher : MonoBehaviourPunCallbacks
         }
     }
 
-
     public override void OnDisconnected(DisconnectCause cause)
     {
         progressLabel.SetActive(false);
@@ -144,31 +165,43 @@ public class Launcher : MonoBehaviourPunCallbacks
         RoomcontrolPanel.SetActive(true);
 
         IsConnecting = false;
-        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+        Debug.LogWarningFormat("OnDisconnected() was called with reason {0}", cause);
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Menu.OpenMenu("Create Room Panel");
+
+        base.OnJoinedLobby();
+        Debug.Log("Joined Lobby");
     }
 
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
-        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+        Debug.Log("OnJoinRandomFailed() was called. No random room available, so we create one.\nReturning: Main Menu");
 
         // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
-        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+        Menu.OpenMenu("Control Panel");
+        //PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
     }
 
     public override void OnJoinedRoom()
     {
-        Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+        Debug.Log("OnJoinedRoom() called, Now this client is in a room.");
 
         // #Critical: We only load if we are the first player, else we rely on `PhotonNetwork.AutomaticallySyncScene` to sync our instance scene.
-        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
-        {
-            Debug.Log("We load the 'Room for 1' ");
+        //if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        //{
+        //    Debug.Log("We load the 'Room for 1' ");
 
 
-            // #Critical
-            // Load the Room Level.
-            PhotonNetwork.LoadLevel("SampleScene");
-        }
+        //    // #Critical
+        //    // Load the Room Level.
+        //    PhotonNetwork.LoadLevel("SampleScene");
+        //}
+
+        PhotonNetwork.LoadLevel("SampleScene");
+        Debug.Log(PhotonNetwork.CurrentRoom.Name);
     }
     #endregion
 }
